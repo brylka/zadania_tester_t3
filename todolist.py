17a04f134e219491efa99c1ebb7b3b79aae50678
Bartosz Bryniarski <brylka@jelenia.com>
2024-05-11 11:03:04 +0200

    Adding method to TodoList class

--- todolist.py
+++ todolist.py
@@ -14,12 +14,15 @@ class TodoList:
         self.tasks = new_tasks
 
     def mark_task_as_done(self, task_id):
-        new_tasks = []
+
+        #self.tasks[task_id-1]['done'] = True
+        # new_tasks = []
         for task in self.tasks:
             if task['id'] == task_id:
                 task['done'] = True
-            new_tasks.append(task)
-        self.tasks = new_tasks
+                break
+            #new_tasks.append(task)
+        #self.tasks = new_tasks
     def get_all_tasks(self):
         return self.tasks
 
@@ -30,8 +33,9 @@ if __name__ == '__main__':
     todolist.add_task("Zadanie testowe")
     #print(todolist.get_all_tasks())
     todolist.add_task("Inne zadanie")
+    todolist.add_task("Trzecie zadanie zadanie")
     #print(todolist.get_all_tasks())
-    #todolist.remove_task(1)
+    todolist.remove_task(1)
     print(todolist.get_all_tasks())
     todolist.mark_task_as_done(2)
     print(todolist.get_all_tasks())
