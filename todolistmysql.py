83334fbcaa180f2ce64578514ea42376c6df71d8
Bartosz Bryniarski <brylka@jelenia.com>
2024-05-12 09:37:49 +0200

    Adding other methods to TodoList class

--- todolistmysql.py
+++ todolistmysql.py
@@ -15,17 +15,24 @@ class TodoList:
         #self.tasks.append({'id' : self.current_id, 'task' : task, 'done' : False})
         #return self.current_id
     def remove_task(self, task_id):
-        new_tasks = []
-        for task in self.tasks:
-            if task['id'] != task_id:
-                new_tasks.append(task)
-        self.tasks = new_tasks
+        sql = "DELETE FROM tasks WHERE id = %s"
+        self.cursor.execute(sql, (task_id))
+        self.db.commit()
+        #new_tasks = []
+        #for task in self.tasks:
+        #    if task['id'] != task_id:
+        #        new_tasks.append(task)
+        #self.tasks = new_tasks
 
     def mark_task_as_done(self, task_id):
-        for task in self.tasks:
-            if task['id'] == task_id:
-                task['done'] = True
-                break
+        sql = "UPDATE tasks SET done = TRUE WHERE id = %s"
+        self.cursor.execute(sql, (task_id))
+        self.db.commit()
+        #for task in self.tasks:
+        #    if task['id'] == task_id:
+        #        task['done'] = True
+        #        break
+
     def get_all_tasks(self):
         sql = "SELECT * FROM tasks"
         self.cursor.execute(sql)
@@ -38,8 +45,13 @@ if __name__ == '__main__':
     todolist = TodoList(db)
 
     print(todolist.get_all_tasks())
-    print(todolist.add_task("Nauka Pythona"))
+    task_id = todolist.add_task("Nauka kasowania")
     print(todolist.get_all_tasks())
+    todolist.mark_task_as_done(task_id)
+    print(todolist.get_all_tasks())
+    todolist.remove_task(task_id)
+    print(todolist.get_all_tasks())
+
 
 
 # CREATE TABLE tasks (id INT NOT NULL AUTO_INCREMENT , task TEXT NOT NULL , done BOOLEAN NOT NULL DEFAULT FALSE , PRIMARY KEY (`id`))
